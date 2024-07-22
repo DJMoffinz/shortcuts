58b1cddfaa13f4d79312cff70fae1e3b9979dce4
DJMoffinz <75518058+DJMoffinz@users.noreply.github.com>
2024-07-22 21:33:57 +1000

    Update shortcuts.sh
    add a bunch of comments

--- shortcuts.sh
+++ shortcuts.sh
@@ -7,33 +7,33 @@ usage() {
     echo "    ytdl <url> [timestamp] [custom name]"
 }
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then # runs if there are no arguments, displays usage and exits
     usage
     exit 1
 fi
 
-COMMAND=""
+COMMAND="" # initialise the variable that stores the command to be executed
 
-while [ "$1" != "" ]; do
-    case $1 in
+while [ "$1" != "" ]; do # while there are still arguments to be processed, do
+    case $1 in # finds commands, only ytdl and the default case is implemented so far
         ytdl ) shift # shifts past argument
                COMMAND+="yt-dlp \"$1\"" # the first parameter is now $1
                shift # past the first parameter (the url)
                if [ "$1" != "" ]; then # optional timestamp parameter
-                   COMMAND+=" --download-sections \"*$1\""
+                   COMMAND+=" --download-sections \"*$1\"" # i don't know why the asterisk needs to be there, but it does
                    shift # past optional second parameter
                fi
-               if [ "$1" != "" ]; then # optional name parameter
+               if [ "$1" != "" ]; then # optional output file name parameter
                    COMMAND+=" -o \"$1 [%(id)s].%(ext)s\""
                fi
-               COMMAND+=" -f \"bestvideo[height<=720][fps<=30]+bestaudio/best\""
-               COMMAND+=" --extractor-args \"youtube:player_client=android,web\""
+               COMMAND+=" -f \"bestvideo[height<=720][fps<=30]+bestaudio/best\"" # this caps the resolution and framerate because it saves space and i only use this for downloading memes anyway
+               COMMAND+=" --extractor-args \"youtube:player_client=android,web\"" # if this wasnt here video probably wouldn't download. see https://github.com/yt-dlp/yt-dlp/issues/8066
                ;;
-        * )    usage
+        * )    usage # default case, handles unexpected input and exits
                exit 1
     esac
     shift
 done
 
-echo "evaluating $COMMAND"
+echo "evaluating $COMMAND" # useful for me, should probably make this exclusive to a debug flag
 eval $COMMAND
