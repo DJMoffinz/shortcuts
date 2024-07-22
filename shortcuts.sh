37b499d52186aa57791f2fa30f09c064bb0ce11c
DJMoffinz <75518058+DJMoffinz@users.noreply.github.com>
2024-07-22 20:13:20 +1000

    Update shortcuts.sh

--- shortcuts.sh
+++ shortcuts.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 usage() {
-    echo "usage: $0 ytdl url [timestamp]"
+    echo "Shortcuts (or sc for short) is a small utility I've written in bash to give me shortcuts to commands I use a lot."
+    echo "usage: sc command <parameters> [optional parameters]"
+    echo "  YouTube Downloader:"
+    echo "    ytdl <url> [timestamp] [custom name]"
 }
 
 if [ $# -eq 0 ]; then
@@ -16,10 +19,14 @@ while [ "$1" != "" ]; do
         ytdl ) shift # shifts past argument
                COMMAND+="yt-dlp \"$1\"" # the first parameter is now $1
                shift # past the first parameter (the url)
-               if [ "$1" != "" ]; then
+               if [ "$1" != "" ]; then # optional timestamp parameter
                    COMMAND+=" --download-sections \"*$1\""
                    shift # past optional second parameter
                fi
+               if [ "$1" != "" ]; then # optional name parameter
+                   COMMAND+=" -o \"$1 [%(id)s].%(ext)s\""
+               fi
+               COMMAND+=" -f \"bestvideo[height<=720][fps<=30]+bestaudio/best\""
                COMMAND+=" --extractor-args \"youtube:player_client=android,web\""
                ;;
         * )    usage
